0ecebd328b16c3c2d9c8e6baa8d0b8308af9527d
Admin <Admin@blore2.com>
2019-12-30 17:01:13 +0530

    commit4

--- JavaN/src/abstractClass1.java
+++ JavaN/src/abstractClass1.java
@@ -3,7 +3,7 @@ public class abstractClass1 {
 
 	public static void main(String args[])
 	{
-		System.out.println("oye git");
+		System.out.println("oye git");System.out.println("oye git");
 	}
 	
 }
